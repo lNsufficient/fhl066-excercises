dff4d1c8af3892723b9486782540b90b43cef06c
Edvard Johansson <edvard.jo@gmail.com>
2015-12-27 10:41:56 +0100

    updated newmark for 9.1 so that it calculates int force for u_np1

--- ovn9.1/newmark_91.m
+++ ovn9.1/newmark_91.m
@@ -1,9 +1,11 @@
 clear;
 data_e9;
+M = plan3gm;
 
-f_int = zeros(ndof,1);  
+f_int = zeros(ndof,1);
+M = zeros(ndof,ndof);
 
-%i det h�r fallet �r g bara den interna kraften, ty C = 0.
+%i det h�r fallet �r g bara den interna kraften, ty C = 0.
 for j = 1:nelm
             index_dof=edof(j,2:end); %de frihg stången gränsar till
             ec=[ex(j,:); ey(j,:)];
@@ -14,15 +16,22 @@ for j = 1:nelm
             D = mstiff(F,mp);
             es = stresscal(F,mp);
             
-            %Ke = plan3ge(ec,t,D,ed,es);
-            %K(index_dof,index_dof)=K(index_dof,index_dof)+Ke;
+            Me = plan3gm(ec, t, rho);
+            M(index_dof) = M(index_dof) + Me;
             
             ef= plan3gf(ec, t, ed, es);
             f_int(index_dof) = f_int(index_dof) + ef;
 end   
 
-%1 - initial conditions
-upp_0 = Minv*(f_0-g(u_0, up_0));
+Minv = inv(M);
+
+%1 - initial conditions. Varje varv ökas väl f_0?
+upp_0 = Minv*(f_0-f_int);
+
+%setting up for iteration
+upp_n = upp_0;
+up_n = a*0;
+u_n = a;
 
 n = 0;
 n_end = 40;
@@ -37,11 +46,32 @@ while(n < n_end)
     du_norm = eps_u + 3;
     while (res > eps_r || du_norm > eps_u)
         %3 - residual calculation
-        r = f_np1 - M*upp_np1-g(u_np1,up_np1);
+        
+        %- beräkning av f-int till residualen.
+        %  denna beräknas mha u_np1 och up_np1
+        f_int = 0*f_int;
+        for j = 1:nelm
+            index_dof=edof(j,2:end); %de frihg stången gränsar till
+            ec=[ex(j,:); ey(j,:)];
+            ed=u_np1(index_dof);
+            [ee,eff]= plan3gs(ec,ed);
+            
+            F = F_vect2tens(eff);
+            D = mstiff(F,mp);
+            es = stresscal(F,mp);
+            
+            %Ke = plan3ge(ec,t,D,ed,es);
+            %K(index_dof,index_dof)=K(index_dof,index_dof)+Ke;
+            
+            ef= plan3gf(ec, t, ed, es);
+            f_int(index_dof) = f_int(index_dof) + ef;
+        end 
+        g_np1 = f_int;
+        r = f_np1 - M*upp_np1-g_np1;
         
         %4 - system matrices and increment correction
         K = getK();
-        C = K*0;  %vi har antagit att det inte beror p� hastigheter
+        C = K*0;  %vi har antagit att det inte beror p� hastigheter
         K_star = K + gamma/(beta*h)*C + 1/(beta*h^2)*M;
         du = solveq(K_star, r, bc);
         u_np1 = u_np1 + du;
